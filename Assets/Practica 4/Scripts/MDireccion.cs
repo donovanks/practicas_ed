d7de3cab333c122bab6418ec39eef85955e1d447
unitec <unitec@unitec.mx>
2019-06-20 11:15:35 -0500

    practica 6
    paracticas de la 1 a la 6

---
+++ Assets/Practica 4/Scripts/MDireccion.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MDireccion : MonoBehaviour
+{
+    //Variables Globales
+
+    float velocidad;
+    Transform target;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        velocidad = 0.5f;
+        target = GameObject.Find("Boris").GetComponent<Transform>();
+        Debug.Log(target.name);
+        Vector3 dir = target.position - this.transform.position;
+        Debug.Log(dir.magnitude);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 dir = target.position - this.transform.position;
+        if (dir.magnitude > 1)
+        {
+            this.transform.Translate(dir.normalized * velocidad, Space.World);
+
+        }
+    }
+}
