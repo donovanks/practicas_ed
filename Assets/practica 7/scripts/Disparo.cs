6e22125642b9b2e70efc37c2d7877836bfd3158a
unitec <unitec@unitec.mx>
2019-06-24 12:49:49 -0500

    practica 7
    se agrego al cañon de un disparo

---
+++ Assets/practica 7/scripts/Disparo.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disparo : MonoBehaviour
+{
+    public Rigidbody bullet;
+    public Transform cañon;
+    public float FuerzaDisparo;
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Rigidbody bulletpos = Instantiate(bullet, cañon.position, cañon.rotation)as Rigidbody;
+
+            bulletpos.AddForce(cañon.forward * FuerzaDisparo);
+
+        }
+
+    }
+}
