5a95d6cc6888cfb570f109fe98a3b466439a6192
donovanks <venom_donovan@hotmail.com>
2019-07-27 18:02:37 -0500

    proyecto
    se agrego el proyecto en su primera fase

---
+++ Assets/REVOLUCION/Assets/Scripts/CamaraMov.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamaraMov : MonoBehaviour
+{
+    public Transform target;
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
+
+
+
+    void LateUpdate()
+    {
+        Vector3 desirePosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desirePosition, smoothSpeed);
+        transform.position = smoothedPosition;
+
+        transform.LookAt(target);
+
+    }
+
+}
